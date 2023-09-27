5b148e566b0b2180fee50368ef3e0af1ad9ed0ea
alvarezalanfranco@gmail.com <alvarezalanfranco@gmail.com>
2023-09-27 14:44:50 -0300

    Creando componente NavBar

---
+++ portfolio-personal/src/Components/NavBar/navbar.jsx
@@ -0,0 +1,19 @@
+//import React from 'react'
+import './navbar.css'
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img src="" alt="Logo" className="logo" />
+      <div className="desktopMenu">
+        <link rel="stylesheet" href="" className="desktopMenuListItem">Home</link>
+        <link rel="stylesheet" href="" className="desktopMenuListItem">Sobre mi</link>
+        <link rel="stylesheet" href="" className="desktopMenuListItem">Portfolio</link>
+        <link rel="stylesheet" href="" className="desktopMenuListItem">Skills</link>
+      </div>
+      <button className="desktopMenuBtn"></button>
+    </div>
+  )
+}
+
+export default NavBar
