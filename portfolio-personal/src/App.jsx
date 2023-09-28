30f0d8fb0209d7d006dd458fe0008dcc44b3872a
alvarezalanfranco@gmail.com <alvarezalanfranco@gmail.com>
2023-09-28 13:16:33 -0300

    Se agregan iconos para porfolio y estilo a barra de navegacion

--- portfolio-personal/src/App.jsx
+++ portfolio-personal/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import NavBar from './Components/NavBar/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
-  const handleClick = () => {
-    setCount(count + 1)
-  }
+ 
 
   return (
     <>
      <NavBar />
-      <h1>Portfolio</h1>
-      <button onClick={handleClick}> {count}</button>
     </>
   )
 }
--- portfolio-personal/src/Components/NavBar/navbar.jsx
+++ portfolio-personal/src/Components/NavBar/navbar.jsx
@@ -1,18 +1,24 @@
 //import React from 'react'
+import {Link} from 'react-scroll'
+
 import './navbar.css'
+import logo from '../../assets/logo.png'
+import contact from '../../assets/contact.png'
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <img src="" alt="Logo" className="logo" />
+    <nav className='navbar'>
+      <img className='logo' src={logo} alt='Logo' />
       <div className="desktopMenu">
-        <link rel="stylesheet" href="" className="desktopMenuListItem">Home</link>
-        <link rel="stylesheet" href="" className="desktopMenuListItem">Sobre mi</link>
-        <link rel="stylesheet" href="" className="desktopMenuListItem">Portfolio</link>
-        <link rel="stylesheet" href="" className="desktopMenuListItem">Skills</link>
+        <Link to='https://www.google.com' className="desktopMenuListItem">Home</Link>
+        <Link to='https://www.google.com' className="desktopMenuListItem">Sobre mi</Link>
+        <Link to='https://www.google.com' className="desktopMenuListItem">Portfolio</Link>
+        <Link to='https://www.google.com' className="desktopMenuListItem">Skills</Link>
       </div>
-      <button className="desktopMenuBtn"></button>
-    </div>
+      <button className='desktopMenuBtn'>
+        <img src={contact} alt='' className='desktopMenuImg' /> ContactMe
+      </button>
+    </nav>
   )
 }
 
