f9c46bba20303b82b82a0ee0d5764e9daae12221
alanfrancoa~ <alvarezalanfranco@gmail.com>
2024-01-13 14:54:47 -0300

    Se agrega enrutado.

--- portfolio-personal/src/App.jsx
+++ portfolio-personal/src/App.jsx
@@ -1,20 +1,26 @@
-//import { useState } from 'react'
-import NavBar from './Components/NavBar/navbar'
-import Intro from './Components/Intro/intro'
-import About from './Components/About/about'
-import Works from './Components/Works/works'
-import Skills from './Components/Skills/Skills'
+// app.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './Components/NavBar/navbar';
+import Intro from './Components/Intro/intro';
+import About from './Components/About/about';
+import Works from './Components/Works/works';
+import Skills from './Components/Skills/Skills';
 
 function App() {
- return (
-    <>
-     <NavBar />
-     <Intro />
-     <About />
-     <Works />
-     <Skills/>
-    </>
-  )
+  return (
+    <Router>
+      <>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Intro} />
+          <Route path="/sobre-mi" component={About} />
+          <Route path="/proyectos" component={Works} />
+          <Route path="/skills" component={Skills} />
+        </Switch>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- portfolio-personal/src/Components/NavBar/navbar.jsx
+++ portfolio-personal/src/Components/NavBar/navbar.jsx
@@ -1,25 +1,23 @@
-//import React from 'react'
-import {Link} from 'react-scroll'
-
-import './navbar.css'
-//import logo from '../../assets/logo.png'
-import contact from '../../assets/contact.png'
+// navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+import contact from '../../assets/contact.png';
 
 const NavBar = () => {
   return (
-    <nav className='navbar'>
-      
+    <nav className="navbar">
       <div className="desktopMenu">
-        <Link to='https://www.google.com' className="desktopMenuListItem">Home</Link>
-        <Link to='https://www.google.com' className="desktopMenuListItem">Sobre mi</Link>
-        <Link to='https://www.google.com' className="desktopMenuListItem">Proyectos</Link>
-        <Link to='https://www.google.com' className="desktopMenuListItem">Skills</Link>
+        <Link to="/" className="desktopMenuListItem">Home</Link>
+        <Link to="/sobre-mi" className="desktopMenuListItem">Sobre mi</Link>
+        <Link to="/proyectos" className="desktopMenuListItem">Proyectos</Link>
+        <Link to="/skills" className="desktopMenuListItem">Skills</Link>
       </div>
-      <button className='desktopMenuBtn'>
-        <img src={contact} alt='' className='desktopMenuImg' /> Contacto
+      <button className="desktopMenuBtn">
+        <img src={contact} alt="" className="desktopMenuImg" /> Contacto
       </button>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
